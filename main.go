2672a495529143aa85cdd4c917c50208b95d2a27
G. Rossin <gpl.rgp.pgl@gmail.com>
2019-08-27 14:27:14 +0200

    More obvious names.

--- main.go
+++ main.go
@@ -3,32 +3,41 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"time"
 
 	"github.com/jelmersnoeck/experiment"
 )
 
 func main() {
-	g1, g2 := newGenerator(2), newGenerator(4)
+	oldGen, newGen := newGenerator(2), newGenerator(4)
 
 	exp := experiment.New(
 		experiment.WithPercentage(50),
-		experiment.WithPublisher(experiment.NewLogPublisher("generator", nil)),
+		// All your "metadata" regarding the experiment will go here as settings for the publisher.
+		experiment.WithPublisher(experiment.NewLogPublisher(fmt.Sprintf("generation-%s", time.Now()), nil)),
 	)
 
 	exp.Control(func() (interface{}, error) {
-		index, err := g1.save(3)
-		fetch, _ := g1.fetch(index)
+		index, err := oldGen.save(3)
+		if err != nil {
+			return nil, err
+		}
+
+		fetch, err := oldGen.fetch(index)
 
 		return fetch, err
 	})
 
 	exp.Candidate("candidate1", func() (interface{}, error) {
-		index, err := g2.save(3)
-		fetch, _ := g2.fetch(index)
+		index, err := newGen.save(3)
+		if err != nil {
+			return nil, err
+		}
+
+		fetch, err := newGen.fetch(index)
 
 		return fetch, err
 	})
-
 	exp.Force(true)
 
 	exp.Compare(func(a, b interface{}) bool {
